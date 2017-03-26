bfe83d4599232473dc96fbc48c3404215200eba8
juani <onofgo@gmail.com>
2017-03-26 08:47:32 -0300

    Se creó un GULP básico para SASS, BrowserSync, HTML y JS. Para compilar SASS, leer HTML, JS y crear un server con Browser-Sync.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+/*
+* Dependencias
+*/
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    browserSync = require("browser-sync").create();
+
+/*
+* Compila los archivos sass y agrega los prefijos propietarios
+*/
+gulp.task("sass", function(){
+  gulp.src("./scss/**/*.scss")
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on("error", sass.logError))
+    .pipe(autoprefixer({
+      versions: ['last 2 browsers']
+    }))
+    .pipe(gulp.dest("./css"));
+});
+
+/*
+* Ejecuta la tarea sass y queda escuchando
+*/
+gulp.task("default", function(){
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("./scss/**/*.scss", ["sass"]).on("change", browserSync.reload);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch("./*.js").on("change", browserSync.reload);
+});
