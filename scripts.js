f9e051f6bd41bc9ddaf9d2aff403b3c8a934b443
juani <onofgo@gmail.com>
2017-05-16 12:50:05 -0300

    Se implemento Responsive Design

--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ boton.addEventListener("click", pesaje);
 var sistemaSolar = [];
 sistemaSolar.push(new Planeta("Mercurio", 2.8));
 sistemaSolar.push(new Planeta("Venus", 8.9));
-sistemaSolar.push(new Planeta("Tierra", 9.81));
 sistemaSolar.push(new Planeta("Marte", 3.71));
 sistemaSolar.push(new Planeta("Jupiter", 22.9));
 sistemaSolar.push(new Planeta("Saturno", 9.1));
@@ -20,9 +19,9 @@ sistemaSolar.push(new Planeta("Urano", 7.8));
 sistemaSolar.push(new Planeta("Neptuno", 11.0));
 
 var pesoUsuario,
-    pesoFinal;
-var gTierra = 9.81;
-var estado = 0;
+    pesoFinal,
+    gTierra = 9.81,
+    estado = 0;
 
 function pesaje(){
   var inputUsuario = document.getElementById("js-usuario");
@@ -37,7 +36,7 @@ function pesaje(){
       pesoFinal = pesoUsuario * w.gravedad / gTierra;
       pesoFinal = Math.round(pesoFinal);
       console.log(pesoFinal + "kg en " + w.nombre);
-      resultado.innerHTML += '<div class="planetario__item"><span class="planetario__item--nombre">' + w.nombre + '</span><span class="planetario__item--peso">' + pesoFinal + ' Kg</span></div>';
+      resultado.innerHTML += '<div class="main-content__item"><span class="main-content__item--name">' + w.nombre + '</span><span class="main-content__item--weight">' + pesoFinal + ' Kg</span></div>';
     }
   }
 
